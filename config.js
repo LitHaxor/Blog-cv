3a557315077b1cd044cc4e4a323347fcafa42ddc
Hasan Ahamed <officials.hasan@gmail.com>
2020-08-04 10:07:09 +0600

    added lib

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  url: 'https://dospolov.com',
+  url: 'https://lithaxor.tech',
   pathPrefix: '/',
-  title: 'Blog by Marat Dospolov',
+  title: 'Blog by Lihaxor',
   keywords:
     'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
-  subtitle: 'Developer. Optimizer. Learner.',
-  copyright: '© 2020 | Kyiv, Ukraine',
-  disqusShortname: 'dospolov',
+  subtitle: 'Developer. Student. Learner.',
+  copyright: '© 2020 | Hasan, Ahamed',
+  disqusShortname: 'Lithaxor',
   postsPerPage: 20,
   googleAnalyticsId: 'UA-6589522-7',
   menu: [
@@ -20,18 +20,18 @@ module.exports = {
     }
   ],
   author: {
-    name: 'Marat Dospolov',
+    name: 'Hasan Ahamed',
     photo: '/photo.jpg',
-    bio: 'Developer. Optimizer. Learner.',
+    bio: 'Developer. Student. Learner.',
     contacts: {
       // don't remove fields, just make them empty string ''
       // https://github.com/gatsbyjs/gatsby/issues/2392
-      github: 'dospolov',
-      twitter: 'marat_dospolov',
-      linkedin: 'dospolov',
-      telegram: 'dospolov',
-      instagram: 'dospolov',
-      facebook: 'dospolov',
+      github: 'lithaxor',
+      twitter: 'lithaxor',
+      linkedin: 'lithaxor',
+      telegram: 'lithaxor',
+      instagram: 'lithaxor',
+      facebook: 'lithaxor',
       email: '',
       rss: '/rss.xml'
     }
